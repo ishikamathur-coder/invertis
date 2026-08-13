9afca8511e61dfb4b95752f0c99201a3b8820f95
Ishika <ishikamathur49@gmail.com>
2026-08-13 12:07:49 +0530

    adding table

---
+++ MyfirstProgram.java
@@ -0,0 +1,15 @@
+class MyfirstProgram {
+
+    public static void main(String args[]) {
+        System.out.println("Hello coders");
+        MySecondClass object = new MySecondClass();
+        object.printMyName();
+    }
+}
+
+class MySecondClass {
+
+    public void printMyName() {
+        System.out.println("hello family");
+    }
+}
